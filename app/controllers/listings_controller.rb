e0b44a185c87e9f2e9d4fc0aa73b913aaeddf2dd
Gloria <shekinahglory92@gmail.com>
2014-06-01 02:12:56 +0300

    Set user permissions

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,8 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :check_user, only: [:edit, :update, :destroy]
+  
   # GET /listings
   # GET /listings.json
   def index
@@ -72,4 +74,10 @@ class ListingsController < ApplicationController
     def listing_params
       params.require(:listing).permit(:name, :description, :price, :image)
     end
+	
+	def check_user
+		if current_user != @listing.user
+		  redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
+		 end
+	end	  
 end
