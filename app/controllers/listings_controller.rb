b91254be90f555960bcdcdcdccb30eebf091bc6e
Gloria <shekinahglory92@gmail.com>
2014-06-01 10:23:15 +0300

    Add seller page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,11 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
   before_filter :check_user, only: [:edit, :update, :destroy]
+  
+  def seller
+    @listing = Listing.where(user: current_user).order("created_at DESC")
+  end
   
   # GET /listings
   # GET /listings.json
