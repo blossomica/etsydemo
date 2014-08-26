8c65f11030fe35158a4c5cb1bca2a9660b79cc92
Gloria <shekinahglory92@gmail.com>
2014-08-26 13:55:02 +0300

    Add Seller page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'pages/about'
   get 'pages/contact'
-  get 'seller' => "listings#seller"
+  get '/seller' => "listings#seller"
   
   root 'listings#index'
 
