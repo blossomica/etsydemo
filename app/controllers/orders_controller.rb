353a6ef35b9ff382d4cfec7594c70a352afc4d77
Gloria <shekinahglory92@gmail.com>
2014-09-21 10:33:26 +0300

    Add order history pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,18 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  # GET /orders
-  # GET /orders.json
-  def index
-    @orders = Order.all
-  end
-
-  # GET /orders/1
-  # GET /orders/1.json
-  def show
-  end
+  
+  
+  def sales
+    @orders = Order.all.where(seller: current_user).order("created_at DESC")
+  end	
+	
+  
+   def purchases
+    @orders = Order.all.where(buyer: current_user).order("created_at DESC")
+  end	 
+  
+  
 
   # GET /orders/new
   def new
@@ -18,9 +20,7 @@ class OrdersController < ApplicationController
 	@listing = Listing.find(params[:listing_id])
   end
 
-  # GET /orders/1/edit
-  def edit
-  end
+ 
 
   # POST /orders
   # POST /orders.json
@@ -45,29 +45,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
